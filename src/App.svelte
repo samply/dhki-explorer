3641eeba09c0fe23ec6c64b2f660a5092aa99ee6
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-08-26 16:29:26 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,231 @@
+<script lang="ts">
+  // import "./app.css";
+  import { onMount } from "svelte";
+  import {
+    setOptions,
+    setCatalogue,
+    getAst,
+    clearSiteResults,
+    buildLibrary,
+    buildMeasure,
+    querySpot,
+    setSiteResult,
+    type SpotResult,
+    type LensOptions,
+    type Catalogue,
+  } from "@samply/lens";
+  import options from "./config/options.json";
+  import catalogue from "./config/catalogue.json";
+  import { measures } from "./lib/measures";
+  import { translateAstToCql } from "$lib/ast-to-cql-translator";
+  import { env } from "$env/dynamic/public";
+  import { SvelteMap } from "svelte/reactivity";
+
+  onMount(() => {
+    let opt: LensOptions = options;
+    if (env.PUBLIC_SPOT_URL) {
+      opt.spotUrl = env.PUBLIC_SPOT_URL;
+    }
+    setOptions(opt);
+
+    setCatalogue(catalogue as Catalogue);
+  });
+
+  let abortController = new AbortController();
+  window.addEventListener("lens-search-triggered", () => {
+    console.log("AST:", JSON.stringify(getAst()));
+
+    abortController.abort();
+    abortController = new AbortController();
+
+    // AST to CQL translation
+    const cql = translateAstToCql(
+      getAst(),
+      false,
+      "DKTK_STRAT_DEF_IN_INITIAL_POPULATION",
+      measures,
+    );
+    const lib = buildLibrary(cql);
+    const measure = buildMeasure(
+      lib.url,
+      measures.map((m) => m.measure),
+    );
+
+    clearSiteResults();
+    const query = btoa(
+      JSON.stringify({
+        lang: "cql",
+        lib,
+        measure,
+      }),
+    );
+    querySpot(query, abortController.signal, (result: SpotResult) => {
+      const site = result.from.split(".")[1];
+      if (result.status === "claimed") {
+        // markSiteClaimed(site);
+      } else if (result.status === "succeeded") {
+        const siteResult = JSON.parse(atob(result.body));
+        setSiteResult(site, siteResult);
+      } else {
+        console.error(
+          `Site ${site} failed with status ${result.status}:`,
+          result.body,
+        );
+      }
+    });
+  });
+
+  const genderHeaders: Map<string, string> = new SvelteMap<string, string>()
+    .set("male", "männlich")
+    .set("female", "weiblich")
+    .set("other", "divers")
+    .set("unknown", "unbekannt");
+
+  const barChartBackgroundColors: string[] = ["#4dc9f6", "#3da4c7"];
+
+  const vitalStateHeaders: Map<string, string> = new SvelteMap<string, string>()
+    .set("lebend", "alive")
+    .set("verstorben", "deceased")
+    .set("unbekannt", "unknown");
+
+  const therapyHeaders: Map<string, string> = new SvelteMap<
+    string,
+    string
+  >().set("medicationStatements", "Sys. T");
+</script>
+
+<!-- <lens-search-button></lens-search-button>
+<lens-query-explain-button></lens-query-explain-button>
+<lens-search-bar></lens-search-bar>
+<lens-catalogue toggle={{collapsable: false}}></lens-catalogue>
+<lens-chart
+    title="Gender distribution"
+    dataKey="gender"
+    chartType="pie"
+    displayLegends="{true}"
+></lens-chart> -->
+
+<header>
+  <div class="header-wrapper">
+    <h1>
+      DKFZ Hector Cancer Institute <br /> at the University Medicine Mannheim
+    </h1>
+    <div class="logo">
+      <img src="../logo-dkfz.svg" alt="DKFZ" />
+    </div>
+    <div class="logo">
+      <img src="../logo_umm.svg" alt="UMM" />
+    </div>
+    <div class="logo">
+      <img src="../logo_mfm_ukm.svg" alt="MFM" />
+    </div>
+    <div class="logo">
+      <img src="../logo-hector.png" alt="Hector" />
+    </div>
+  </div>
+</header>
+<main>
+  <div class="search">
+    <div class="search-wrapper">
+      <lens-search-bar noMatchesFoundMessage="keine Ergebnisse gefunden"
+      ></lens-search-bar>
+      <lens-query-explain-button></lens-query-explain-button>
+      <lens-search-button title="Suchen"></lens-search-button>
+    </div>
+  </div>
+  <div class="grid">
+    <div class="catalogue-wrapper">
+      <div class="catalogue">
+        <lens-catalogue
+          toggleIconUrl="right-arrow-svgrepo-com.svg"
+          toggle={{ collapsable: false }}
+          addIconUrl="long-right-arrow-svgrepo-com.svg"
+        ></lens-catalogue>
+      </div>
+    </div>
+    <div class="charts">
+      <div class="chart-wrapper result-summary">
+        <lens-result-summary></lens-result-summary>
+        <lens-search-modified-display
+          >Diagramme repräsentieren nicht mehr die aktuelle Suche!</lens-search-modified-display
+        >
+      </div>
+      <div class="chart-wrapper chart-diagnosis">
+        <lens-chart
+          title="Diagnose"
+          catalogueGroupCode="diagnosis"
+          chartType="bar"
+          indexAxis="y"
+          groupingDivider="."
+          groupingLabel=".%"
+          filterRegex="^[CD].*"
+          xAxisTitle="Anzahl der Diagnosen"
+          yAxisTitle="ICD-10-Codes"
+          backgroundColor={barChartBackgroundColors}
+        ></lens-chart>
+      </div>
+      <div class="chart-wrapper">
+        <lens-chart
+          title="Geschlecht"
+          catalogueGroupCode="gender"
+          chartType="pie"
+          displayLegends={true}
+          headers={genderHeaders}
+        ></lens-chart>
+      </div>
+      <div class="chart-wrapper chart-age-distribution">
+        <lens-chart
+          title="Alter bei Erstdiagnose"
+          catalogueGroupCode="age_at_diagnosis"
+          chartType="bar"
+          groupRange={10}
+          filterRegex="^(([0-9]?[0-9]$)|(1[0-2]0))"
+          xAxisTitle="Alter"
+          yAxisTitle="Anzahl der Primärdiagnosen"
+          backgroundColor={barChartBackgroundColors}
+        ></lens-chart>
+      </div>
+      <div class="chart-wrapper">
+        <lens-chart
+          title="Vitalstatus"
+          catalogueGroupCode="75186-7"
+          chartType="pie"
+          displayLegends={true}
+          headers={vitalStateHeaders}
+        ></lens-chart>
+      </div>
+      <div class="chart-wrapper">
+        <lens-chart
+          title="Therapieart"
+          catalogueGroupCode="therapy_of_tumor"
+          chartType="bar"
+          headers={therapyHeaders}
+          xAxisTitle="Art der Therapie"
+          yAxisTitle="Anzahl der Therapieeinträge"
+          backgroundColor={barChartBackgroundColors}
+        ></lens-chart>
+      </div>
+      <div class="chart-wrapper">
+        <lens-chart
+          title="Proben"
+          catalogueGroupCode="sample_kind"
+          chartType="bar"
+          xAxisTitle="Probentypen"
+          yAxisTitle="Probenanzahl"
+          filterRegex="^(?!(tissue-other|buffy-coat|peripheral-blood-cells|dried-whole-blood|swab|ascites|stool-faeces|saliva|liquid-other|derivative-other))"
+          backgroundColor={barChartBackgroundColors}
+        >
+        </lens-chart>
+      </div>
+    </div>
+  </div>
+</main>
+
+<footer>
+  <div>
+    Made with ♥ and <a href="https://github.com/samply/lens">samply/lens</a>.
+  </div>
+  <div class="logo logo-footer">
+    <img src="../dktk-en.png" alt="DKTK" />
+  </div>
+</footer>
---
+++ src/lib/measures.ts
@@ -0,0 +1,605 @@
+import type { FhirMeasureItem } from "@samply/lens";
+
+const dktkPatientsMeasure: FhirMeasureItem = {
+  key: 'patients',
+  measure: {
+    code: {
+      text: 'patients',
+    },
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'InInitialPopulation',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'Gender',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'Gender',
+        },
+      },
+      {
+        code: {
+          text: '75186-7',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'Deceased',
+        },
+      },
+      {
+        code: {
+          text: 'Age',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'AgeClass',
+        },
+      },
+    ],
+  },
+  cql: `
+DKTK_STRAT_GENDER_STRATIFIER
+
+DKTK_STRAT_AGE_STRATIFIER
+
+DKTK_STRAT_DECEASED_STRATIFIER
+`,
+};
+
+const dktkDiagnosisMeasure: FhirMeasureItem = {
+  key: 'diagnosis',
+  measure: {
+    code: {
+      text: 'diagnosis',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'Condition',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'Diagnosis',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'diagnosis',
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'DiagnosisCode',
+        },
+      },
+    ],
+  },
+  cql: `
+DKTK_STRAT_DIAGNOSIS_STRATIFIER
+`,
+};
+
+const dktkSpecimenMeasure: FhirMeasureItem = {
+  key: 'specimen',
+  measure: {
+    code: {
+      text: 'specimen',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'Specimen',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'Specimen',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'sample_kind',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'SampleType',
+        },
+      },
+    ],
+  },
+  cql: `
+DKTK_STRAT_SPECIMEN_STRATIFIER
+`,
+};
+
+const dktkProceduresMeasure: FhirMeasureItem = {
+  key: 'procedures',
+  measure: {
+    code: {
+      text: 'procedures',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'Procedure',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'Procedure',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'ProcedureType',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'ProcedureType',
+        },
+      },
+    ],
+  },
+  cql: `
+DKTK_STRAT_PROCEDURE_STRATIFIER
+`,
+};
+
+const dktkMedicationStatementsMeasure: FhirMeasureItem = {
+  key: 'medicationStatements',
+  measure: {
+    code: {
+      text: 'medicationStatements',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'MedicationStatement',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'MedicationStatement',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'MedicationType',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'ProcedureType',
+        },
+      },
+    ],
+  },
+  cql: `
+DKTK_STRAT_MEDICATION_STRATIFIER
+`,
+};
+
+const dhkiEncountersMeasure: FhirMeasureItem = {
+  key: "encounters",
+  measure:
+    {
+      "code": {
+        "text": "Encounters"
+      },
+      "extension": [
+        {
+          "url": "http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis",
+          "valueCode": "Encounter"
+        }
+      ],
+      "population": [
+        {
+          "code": {
+            "coding": [
+              {
+                "system": "http://terminology.hl7.org/CodeSystem/measure-population",
+                "code": "initial-population"
+              }
+            ]
+          },
+          "criteria": {
+            "language": "text/cql-identifier",
+            "expression": "Encounter"
+          }
+        }
+      ],
+      "stratifier": [
+        {
+          "code": {
+            "text": "Departments"
+          },
+          "criteria": {
+            "language": "text/cql",
+            "expression": "Departments"
+          }
+        }
+      ]
+    },
+  cql:
+  `
+define Encounter:
+if InInitialPopulation then [Encounter] else {} as List<Encounter>
+
+define function Departments(encounter FHIR.Encounter):
+encounter.identifier.where(system = 'http://dktk.dkfz.de/fhir/sid/hki-department').value.first()
+
+`
+}
+
+const dhkiEncountersMeasureProd: FhirMeasureItem = {
+  key: "encounters",
+  measure:
+    {
+      "code": {
+        "text": "Encounters"
+      },
+      "extension": [
+        {
+          "url": "http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis",
+          "valueCode": "Encounter"
+        }
+      ],
+      "population": [
+        {
+          "code": {
+            "coding": [
+              {
+                "system": "http://terminology.hl7.org/CodeSystem/measure-population",
+                "code": "initial-population"
+              }
+            ]
+          },
+          "criteria": {
+            "language": "text/cql-identifier",
+            "expression": "Encounter"
+          }
+        }
+      ],
+      "stratifier": [
+        {
+          "code": {
+            "text": "Departments"
+          },
+          "criteria": {
+            "language": "text/cql",
+            "expression": "Departments"
+          }
+        }
+      ]
+    },
+  cql:
+  `
+DHKI_STRAT_ENCOUNTER_STRATIFIER
+`
+}
+
+const dhkiSpecimenMeasure: FhirMeasureItem = {
+  key: 'specimen',
+  measure: {
+    code: {
+      text: 'specimen',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'Specimen',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'Specimen',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'sample_kind',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'SampleType',
+        },
+      },
+      {
+        code: {
+          text: 'sample_subtype',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'SampleSubtype',
+        },
+      },
+      // {
+      //   code: {
+      //     text: 'sample_technology',
+      //   },
+      //   criteria: {
+      //     language: 'text/cql',
+      //     expression: 'SampleTechnology',
+      //   },
+      // },
+    ],
+  },
+  cql: `
+define Specimen:
+if InInitialPopulation then [Specimen] else {} as List<Specimen>
+
+define function SampleType(specimen FHIR.Specimen):
+specimen.type.coding.where(system = 'https://fhir.bbmri.de/CodeSystem/SampleMaterialType').code.first()
+
+define function SampleSubtype(specimen FHIR.Specimen):
+specimen.type.text.first()
+
+`,
+};
+
+const dhkiSpecimenMeasureProd: FhirMeasureItem = {
+  key: 'specimen',
+  measure: {
+    code: {
+      text: 'specimen',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'Specimen',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'Specimen',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'sample_kind',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'SampleType',
+        },
+      },
+      {
+        code: {
+          text: 'sample_subtype',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'SampleSubtype',
+        },
+      },
+      // {
+      //   code: {
+      //     text: 'sample_technology',
+      //   },
+      //   criteria: {
+      //     language: 'text/cql',
+      //     expression: 'SampleTechnology',
+      //   },
+      // },
+    ],
+  },
+  cql: `
+DHKI_STRAT_SPECIMEN_STRATIFIER
+`,
+};
+
+const dhkiMedicationStatementsMeasure: FhirMeasureItem = {
+  key: 'medicationStatements',
+  measure: {
+    code: {
+      text: 'medicationStatements',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'MedicationStatement',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'MedicationStatement',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'MedicationType',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'ProcedureType',
+        },
+      },
+      {
+        code: {
+          text: 'AppliedMedications',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'AppliedMedications',
+        },
+      },
+    ],
+  },
+  cql: `
+define MedicationStatement:
+if InInitialPopulation then [MedicationStatement] else {} as List <MedicationStatement>
+
+define function AppliedMedications(medication FHIR.MedicationStatement):
+medication.medication.coding.code.last()
+`,
+}
+
+const dhkiMedicationStatementsMeasureProd: FhirMeasureItem = {
+  key: 'medicationStatements',
+  measure: {
+    code: {
+      text: 'medicationStatements',
+    },
+    extension: [
+      {
+        url: 'http://hl7.org/fhir/us/cqfmeasures/StructureDefinition/cqfm-populationBasis',
+        valueCode: 'MedicationStatement',
+      },
+    ],
+    population: [
+      {
+        code: {
+          coding: [
+            {
+              system:
+                'http://terminology.hl7.org/CodeSystem/measure-population',
+              code: 'initial-population',
+            },
+          ],
+        },
+        criteria: {
+          language: 'text/cql-identifier',
+          expression: 'MedicationStatement',
+        },
+      },
+    ],
+    stratifier: [
+      {
+        code: {
+          text: 'MedicationType',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'ProcedureType',
+        },
+      },
+      {
+        code: {
+          text: 'AppliedMedications',
+        },
+        criteria: {
+          language: 'text/cql',
+          expression: 'AppliedMedications',
+        },
+      },
+    ],
+  },
+  cql: `
+DHKI_STRAT_MEDICATION_STRATIFIER
+`,
+}
+
+export const measures: FhirMeasureItem[] = [
+    dktkPatientsMeasure,
+      dktkDiagnosisMeasure,
+      dhkiSpecimenMeasureProd,
+      dktkProceduresMeasure,
+      dhkiMedicationStatementsMeasureProd,
+      dhkiEncountersMeasureProd,
+];
